da9340d201145b1e569891529595c22098aef1c5
GabrielMatiasn10 <gm4037003@gmail.com>
2023-10-14 15:04:52 -0300

    Termino

---
+++ src/components/Sobre/Sobre.jsx
@@ -0,0 +1,25 @@
+import React from "react";
+import "./Sobre.css";
+
+function Sobre() {
+    return (
+        <section className="sobre-nos">
+        <div className="sobre-nos-content">
+          <h2>Sobre nós</h2>
+          <img
+            src="https://beautvip.com/wp-content/uploads/2021/06/sonhar-cortando-cabelo-2048x1365.jpg"
+            alt="Foto do interior do nosso salão"
+          />
+          <p className="sobre">
+            Bem-vindo ao nosso salão! Nós nos dedicamos a oferecer os melhores
+            tratamentos para cabelo e beleza em um ambiente relaxante e
+            acolhedor. Com uma equipe experiente e apaixonada, estamos empenhados
+            em ajudar você a realçar sua beleza natural.
+          </p>
+       
+        </div>
+      </section>
+    );  
+}
+
+export default Sobre;
