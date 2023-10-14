dda445b3be027e7cc0b31274da55d2741f7ea299
GabrielMatiasn10 <gm4037003@gmail.com>
2023-10-14 14:02:10 -0300

    Termino do Inicio

---
+++ src/components/Inicio/Inicio.jsx
@@ -0,0 +1,21 @@
+import React from "react";
+import "./Inicio.css";
+
+
+function Inicio() {
+  return (
+    <><div className="nomes">
+       <img src="https://protectionforbeauty.volkdobrasil.com.br/blog/wp-content/uploads/2019/02/treinamento-de-funcionarios-no-salao-de-beleza-5-motivos-para-fazer.jpeg" alt="imagem de uma mulher com cabelo cacheado" className="imagem"/>
+      <h1>Saúde natural para os seus cabelos</h1>
+      <p>Um salão exclusivo em Recife, especializado em tratamentos naturais.</p>
+      <button>Agende Um Horario</button>
+    </div>
+    <div>
+     
+    </div>
+    </>
+    
+  );
+}
+
+export default Inicio;
