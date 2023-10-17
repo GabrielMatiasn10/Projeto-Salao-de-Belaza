0c76aa7fed87e15ae6191d714c5862753718c67e
GabrielMatiasn10 <gm4037003@gmail.com>
2023-10-16 23:37:38 -0300

    add

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,19 @@
-import React from 'react';
 import './App.css';
+import React from "react";
+import Header from "./components/Header/Header";
+import Inicio from "./components/Inicio/Inicio";
+import Sobre from "./components/Sobre/Sobre";
+import Servicos from "./components/Servicos/Servicos";
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-       
-      </header>
-    </div>
+   <div>
+    <Header />
+    <Inicio />
+    <Sobre />
+    <Servicos />
+   </div>
   );
 }
 
---
+++ src/components/Depoimentos/Depoimentos.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import "./Depoimentos.css";
+
+function Depoimentos() {
+    return (
+        <div>
+            
+        </div>
+    );
+}
+
+export default Depoimentos;
