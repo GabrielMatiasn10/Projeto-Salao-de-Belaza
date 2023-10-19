355ca45954a1fbb25ed94bd9dcc3a1a758dcdbf9
GabrielMatiasn10 <gm4037003@gmail.com>
2023-10-19 12:03:41 -0300

    Finalizando o Depoimento

--- src/components/Depoimentos/Depoimentos.jsx
+++ src/components/Depoimentos/Depoimentos.jsx
@@ -2,11 +2,23 @@ import React from "react";
 import "./Depoimentos.css";
 
 function Depoimentos() {
-    return (
-        <div>
-            
-        </div>
-    );
+  return (
+    <>
+      <div className="depoimento">
+        <h1>Depoimentos de quem já passou por aqui</h1>
+      </div>
+      <div className="experiencia">
+        <p>
+          Eu sou cliente do Beautysalon há 5 anos e não troco por nada! Certamente meu cabelo mudou muito depois que comecei a tratar somente com produtos naturais e veganos. São profissionais incríveis e qualificados.
+          <img src="https://i0.wp.com/estudos.gospelmais.com.br/files/2012/10/papel_mulher_crista.jpg?fit=1024%2C768&ssl=1" alt="" /> Wanessa Souza
+        </p>
+        <p>
+          Minha mãe frequenta o salão há anos e me levou um dia para conhecer. Minha experiência foi incrível, eu continuo fazendo o a terapia capilar e isso salvou o meu cabelo. Adoro todos os profissionais do Beautysalon.
+          <img src="https://riclan.com.br/wp-content/uploads/2019/02/mulher.png" alt="" /> Luna Falcão
+        </p>
+      </div>
+    </>
+  );
 }
 
 export default Depoimentos;
