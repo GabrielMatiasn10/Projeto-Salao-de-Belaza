2b0862a324360bd66a3c49b679cdee0a9fee3bc4
GabrielMatiasn10 <gm4037003@gmail.com>
2023-10-19 15:43:47 -0300

    Finalizando Contato

---
+++ src/components/Contato/Contato.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+import "./Contato.css";
+
+
+function Contato() {
+  return (
+    <div className="contato-whats">
+      <h1 className="nome">Entre em contato!</h1>
+      <p className="paragrafo">Entre em contato com a Beautysalon, queremos tirar suas dúvidas, ouvir suas críticas e sugestões.</p>
+      <button className="botao">Entrar em contato</button>
+    
+    </div>
+  );
+}
+
+export default Contato;
