3aa0e6d16600fcaf871db9ff5fffc4bd355d9f72
GabrielMatiasn10 <gm4037003@gmail.com>
2024-04-09 11:18:56 -0300

    teste final

--- src/components/Servicos/Servicos.jsx
+++ src/components/Servicos/Servicos.jsx
@@ -9,18 +9,6 @@ function Servicos() {
       <p className="text-servicos">
         Com mais de 10 anos no mercado, o Beautysalon já conquistou clientes de inúmeros países com seus tratamentos exclusivos e totalmente naturais.
       </p>
-      <div className="gallery">
-        <div className="photos">
-          <img src="https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Fi.pinimg.com%2F736x%2F6a%2F3a%2F8a%2F6a3a8a32a7e28374afaaa59b9a5ae00b.jpg&f=1&nofb=1&ipt=e0e6e13e64e808a4502c0a21f224c4b71e414bb8d7237d09a58bb1e8146bec9b&ipo=images" alt="" />
-        </div>
-        <div className="photos">
-          <img src="https://pixabay.com/pt/photos/spa-mulher-facial-beleza-sal%C3%A3o-4481538/" alt="" />
-          </div>
-        <div className="photos">
-          <img src="https://cdn.pixabay.com/photo/2018/04/03/23/04/woman-3288365_640.jpg" alt="" />
-          </div>
-      </div>
-      <img src="https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Fi.pinimg.com%2Foriginals%2F01%2F5e%2Fca%2F015eca74d528eb4e48300317b9a6d5be.jpg&f=1&nofb=1&ipt=7dadfdb8483767e7f3b42fc6ddb3e3b1843a102671be70a04a7a45dbedee6143&ipo=images" alt="" className="imagem" />
       <div className="servicos__container">
         <ServiceItem
            iconName={FiSun}
@@ -38,6 +26,29 @@ function Servicos() {
           description="O beautysalon conta com diversos tratamentos naturais e totalmente veganos, para qualquer tipo de cabelo."
         />
       </div>
+      <div className="gallery">
+  <div className="photos">
+    <img
+      src="https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Fsofthair.com.br%2Fwp-content%2Fuploads%2F2019%2F09%2F307614-treinamento-e-desenvolvimento-para-os-colaboradores-do-salao-de-beleza-encantar-os-clientes.jpg&f=1&nofb=1&ipt=ddf464e28d432797eea64a24bf6879a3022d74b1537e4798f97f93109f6f64ef&ipo=images"
+      className="photo"
+    />
+  </div>
+ 
+  <div className="photos">
+    <img
+      src="https://cdn.pixabay.com/photo/2018/04/03/23/04/woman-3288365_640.jpg"
+      alt=""
+      className="photo"
+    />
+  </div>
+  <div className="photos">
+    <img
+      src="https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Fi.pinimg.com%2Foriginals%2F01%2F5e%2Fca%2F015eca74d528eb4e48300317b9a6d5be.jpg&f=1&nofb=1&ipt=7dadfdb8483767e7f3b42fc6ddb3e3b1843a102671be70a04a7a45dbedee6143&ipo=images"
+      alt=""
+      className="photo"
+    />
+  </div>
+</div>
     </div>
   );
 }
